da22445add14207fc691d83fd6256c4943f2ea36
aiikendoit <akdisrael15@gmail.com>
2023-02-07 23:56:02 +0800

    update file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
